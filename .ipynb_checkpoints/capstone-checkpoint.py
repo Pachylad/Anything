3c87d42c7db43945df249ea79af5b988ba30d2a4
Pachylad <lowjared@gmail.com>
2024-09-14 11:59:32 +0800

    update

--- .ipynb_checkpoints/capstone-checkpoint.py
+++ .ipynb_checkpoints/capstone-checkpoint.py
@@ -17,12 +17,16 @@ thirtythou_new = pd.DataFrame(thirtythou_new_dict)
 
 similarity = cosine_similarity(thirtythou_new)
 
+
 def recommend(song, artist):
     index = thirty_thou[(thirty_thou['track_name'] == song) & (thirty_thou['track_artist'] == artist)].index[0]
     distances = sorted(list(enumerate(similarity[index])),reverse=True,key = lambda x: x[1])
+    songs = []
+    artists = []
     for i in distances[1:6]:
-        print(thirty_thou.iloc[i[0]]['track_name'] + ' by ' + thirty_thou.iloc[i[0]]['track_artist'])
-
+        songs.append(thirty_thou.iloc[i[0]]['track_name'])
+        artists.append(thirty_thou.iloc[i[0]]['track_artist'])
+    return songs, artists
 
 # def fetch_artist?(movie_id):
 #    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}')
@@ -47,13 +51,14 @@ selected_artist_name = my_expander.selectbox("",thirty_thou["track_artist"].valu
 if my_expander.button("Recommend"):
     st.text("Here are few Recommendations..")
     st.write("#")
-    songs, artists = recommend(selected_song_name,selected_artist_name)
+    songs, artists = recommend(selected_song_name, selected_artist_name)
     col1,col2,col3,col4,col5=st.columns(5)
     cols=[col1,col2,col3,col4,col5]
     for i in range(0,5):
             with cols[i]:
                 st.write(f' <b style="color:#E50914"> {songs[i]} </b>',unsafe_allow_html=True)
                 st.write(f' <b style="color:#E50914"> {artists[i]} </b>',unsafe_allow_html=True)
+                #st.write(f' <b style="color:#E50914"> {names[i]} </b>',unsafe_allow_html=True)
                 
 
 
